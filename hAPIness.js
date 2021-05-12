e7b589c37801ffce3ed675b463fa2ec2531f6fa2
gregoire renaldo <gregoire.renaldo@gmail.com>
2021-05-12 13:04:11 +0200

    aaa

--- hAPIness.js
+++ hAPIness.js
@@ -28,7 +28,7 @@ app.use(express.static('public'));
 
 app.post('/verify', (req, res) => {
   console.log(PORT)
-  console.log(req)
+  console.log(req.body)
   res.status(200).json({
     "challenge": req.body.challenge
   });
