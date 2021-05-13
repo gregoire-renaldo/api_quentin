6f1215a92e5f88549c6f319bd7248e505264b011
gregoire renaldo <gregoire.renaldo@gmail.com>
2021-05-13 12:55:14 +0200

    ___

--- hAPIness.js
+++ hAPIness.js
@@ -44,7 +44,7 @@ app.use(express.json());
 app.post('/open', function (request, response) {
   console.log('avant le if, dans /open')
   console.log(request.body)
-  if (request.body.event.type == "home") {
+  if (request.body.event.type == "app_home_opened") {
     console.log("opennnn")
     const data = {
       token: request.body.token,
