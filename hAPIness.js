b9350e2232f9ae21b08f79a9240a55ccb2eb349d
gregoire renaldo <gregoire.renaldo@gmail.com>
2021-05-17 15:39:59 +0200

    ee

--- hAPIness.js
+++ hAPIness.js
@@ -14,14 +14,14 @@ const axios = require('axios');
 const PORT = process.env.PORT || 3000
 const app = express();
 // Démarrer le serveur
-// app.listen(PORT, () => console.log(`Listening on ${PORT}`))
+app.listen(PORT, () => console.log(`Listening on ${PORT}`))
 
 // TO set for slack verif, then to comment
 // Start a basic HTTP server
-slackEvents.start(PORT).then(() => {
-  // Listening on path '/slack/events' by default
-  console.log(`server listening on port ${PORT}`);
-});
+// slackEvents.start(PORT).then(() => {
+//   // Listening on path '/slack/events' by default
+//   console.log(`server listening on port ${PORT}`);
+// });
 
 app.use(express.static('public'));
 
@@ -158,85 +158,3 @@ app.post('/open', function (request, response) {
     response.send(' what ?');
   }
 })
-
-// TEST open action solo
-// app.post('/open', function (request, response) {
-//   req =request
-//   console.log('avant le if, dans /open request =' + request)
-//   console.log('type d objet' + ' ' + typeof request)
-//   console.log('request body'+' '+request.body)
-//   console.log(request.body);
-//   console.log(request.body.type);
-//   const data = {
-//     type: request.body.type,
-//     user_id: request.body.user.id,
-//     user_username: request.body.user.username,
-//     user_name: request.body.user.name,
-//     user_team_id: request.body.user.team_id,
-//     api_app_id: request.body.api_app_id,
-//     token: request.body.token,
-//     container_type: request.body.container.type,
-//     container_message: request.body.container.message,
-//     container_channel_id: request.body.container.channel_id,
-//     container_is_ephemmeral: request.body.container.container_is_ephemmeral,
-//     trigger_id: request.body.trigger_id,
-//     team_id: request.body.team.id,
-//     team_domain: request.body.team.domain,
-//     enterprise: request.body.enterprise,
-//     is_enterprise_install: request.body.is_enterprise_install,
-//     channel_id: request.body.channel.id,
-//     channel_name: request.body.channel.name,
-//     message_bot_id: request.body.message.bot_id,
-//     message_type: request.body.message.type,
-//     message_text: request.body.message.text,
-//     message_user: request.body.message.user,
-//     message_team: request.body.message.team,
-//     message_blocks_block_1_type: request.body.message.blocks[0].type,
-//     message_blocks_block_1_block_id: request.body.message.blocks[0].block_id,
-//     message_blocks_block_1_text_type: request.body.message.blocks[0].text.type,
-//     message_blocks_block_1_text_text: request.body.message.blocks[0].text.text,
-//     message_blocks_block_1_text_emoji: request.body.message.blocks[0].text.emoji,
-//     message_blocks_block_2_type: request.body.message.blocks[1].type,
-//     message_blocks_block_2_block_id: request.body.message.blocks[1].block_id,
-//     message_blocks_block_2_text_type: request.body.message.blocks[1].text.type,
-//     message_blocks_block_2_text_text: request.body.message.blocks[1].text.text,
-//     message_blocks_block_2_text_verbatim: request.body.message.blocks[1].text.verbatim,
-//     message_blocks_block_2_accessory_type: request.body.message.blocks[1].accessory.type,
-//     message_blocks_block_2_accessory_image_url: request.body.message.blocks[1].accessory.image_url,
-//     message_blocks_block_2_accessory_alt_text: request.body.message.blocks[1].accessory.alt_text,
-//     message_blocks_block_3_type: request.body.message.blocks[2].type,
-//     message_blocks_block_3_block_id: request.body.message.blocks[2].block_id,
-//     message_blocks_block_3_elements_1_type: request.body.message.blocks[2].elements[0].type,
-//     message_blocks_block_3_elements_1_action_id: request.body.message.blocks[2].elements[0].action_id,
-//     message_blocks_block_3_elements_1_text_type: request.body.message.blocks[2].elements[0].text.type,
-//     message_blocks_block_3_elements_1_text_text: request.body.message.blocks[2].elements[0].text.text,
-//     message_blocks_block_3_elements_1_text_emoji: request.body.message.blocks[2].elements[0].text.emoji,
-//     message_blocks_block_3_elements_1_style: request.body.message.blocks[2].elements[0].style,
-//     message_blocks_block_3_elements_1_value: request.body.message.blocks[2].elements[0].value,
-//     message_blocks_block_3_elements_1_url: request.body.message.blocks[2].elements[0].url,
-//     state_values: request.body.state.values,
-//     response_url: request.body.response_url,
-//     actions_1_action_id: request.body.actions[0].action_id,
-//     actions_1_block_id: request.body.actions[0].block_id,
-//     actions_1_text_type: request.body.actions[0].text.type,
-//     actions_1_text_text: request.body.actions[0].text.text,
-//     actions_1_text_emoji: request.body.actions[0].text.emoji,
-//     actions_1_value: request.body.actions[0].value,
-//     actions_1_style: request.body.actions[0].style,
-//     actions_1_type: request.body.actions[0].type,
-//     actions_1_action_ts: request.body.actions[0].action_ts,
-//   }
-//   response.json(data)
-//   // change the url for bubble's url
-//   axios.post('https://joypool12.bubbleapps.io/version-test/api/1.1/wf/endpoint-rc/', data)
-//     .then((res) => {
-//       // console.log(res.headers);
-//       // console.log(`Status: ${res.status}`);
-//       console.log('Body: ' + res.data);
-//     }).catch((err) => {
-//       console.error(err);
-//     });
-// });
-
-
-
