35ce5d9a70d4e1d642ba9fb470865cf93605ceec
gregoire renaldo <gregoire.renaldo@gmail.com>
2021-05-18 17:04:48 +0200

    z

--- hAPIness.js
+++ hAPIness.js
@@ -28,7 +28,6 @@ app.use(express.static('public'));
 // slackEvents.on('message', (event) => {
 //   console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
 // });
-
 // Handle errors (see `errorCodes` export)
 slackEvents.on('error', console.error);
 
@@ -136,110 +135,12 @@ app.post('/open', function (request, response) {
       actions_1_type: slack_payload.actions[0].type,
       actions_1_action_ts: slack_payload.actions[0].action_ts,
     }
-
-
     console.log('response'+' '+ response)
     axios.post('https://joypool12.bubbleapps.io/version-test/api/1.1/wf/endpoint-action/', dataPost)
       .then((res) => {
       }).catch((err) => {
         console.error(err);
       });
-        // console.log(resp.data);
-
     };
   }
 )
-
-
-    // console.log(response)
-    // change the url for bubble's url
-    // axios.post('https://joypool12.bubbleapps.io/version-test/api/1.1/wf/endpoint-rc/initialize', data)
-    // response.status(200).send('OK')
-    // axios.post('https://joypool12.bubbleapps.io/version-test/api/1.1/wf/endpoint-action/', data);
-    // .then((res) => {
-    //     console.log('response'+ ' '+response)
-    //   }).catch((err) => {
-    //     console.error(err);
-    //   });
-
-
-
-// app.post('/open', function (request, response) {
-//   const slack_payload = JSON.parse(request.body.payload);
-//   console.log(slack_payload)
-//     const data = {
-//       type: slack_payload.type,
-//       user_id: slack_payload.user.id,
-//       user_username: slack_payload.user.username,
-//       user_name: slack_payload.user.name,
-//       user_team_id: slack_payload.user.team_id,
-//       api_app_id: slack_payload.api_app_id,
-//       token: slack_payload.token,
-//       container_type: slack_payload.container.type,
-//       container_message: slack_payload.container.message,
-//       container_channel_id: slack_payload.container.channel_id,
-//       container_is_ephemmeral: slack_payload.container.container_is_ephemmeral,
-//       trigger_id: slack_payload.trigger_id,
-//       team_id: slack_payload.team.id,
-//       team_domain: slack_payload.team.domain,
-//       enterprise: slack_payload.enterprise,
-//       is_enterprise_install: slack_payload.is_enterprise_install,
-//       channel_id: slack_payload.channel.id,
-//       channel_name: slack_payload.channel.name,
-//       message_bot_id: slack_payload.message.bot_id,
-//       message_type: slack_payload.message.type,
-//       message_text: slack_payload.message.text,
-//       message_user: slack_payload.message.user,
-//       message_ts: slack_payload.message.ts,
-//       message_team: slack_payload.message.team,
-//       // message_blocks_block_1_type: slack_payload.message.blocks[0].type,
-//       // message_blocks_block_1_block_id: slack_payload.message.blocks[0].block_id,
-//       // message_blocks_block_1_text_type: slack_payload.message.blocks[0].text.type,
-//       // message_blocks_block_1_text_text: slack_payload.message.blocks[0].text.text,
-//       // message_blocks_block_1_text_emoji: slack_payload.message.blocks[0].text.emoji,
-//       // message_blocks_block_2_type: slack_payload.message.blocks[1].type,
-//       // message_blocks_block_2_block_id: slack_payload.message.blocks[1].block_id,
-//       // message_blocks_block_2_text_type: slack_payload.message.blocks[1].text.type,
-//       // message_blocks_block_2_text_text: slack_payload.message.blocks[1].text.text,
-//       // message_blocks_block_2_text_verbatim: slack_payload.message.blocks[1].text.verbatim,
-//       // message_blocks_block_2_accessory_type: slack_payload.message.blocks[1].accessory.type,
-//       // message_blocks_block_2_accessory_image_url: slack_payload.message.blocks[1].accessory.image_url,
-//       // message_blocks_block_2_accessory_alt_text: slack_payload.message.blocks[1].accessory.alt_text,
-//       // message_blocks_block_3_type: slack_payload.message.blocks[2].type,
-//       // message_blocks_block_3_block_id: slack_payload.message.blocks[2].block_id,
-//       // message_blocks_block_3_elements_1_type: slack_payload.message.blocks[2].elements[0].type,
-//       // message_blocks_block_3_elements_1_action_id: slack_payload.message.blocks[2].elements[0].action_id,
-//       // message_blocks_block_3_elements_1_text_type: slack_payload.message.blocks[2].elements[0].text.type,
-//       // message_blocks_block_3_elements_1_text_text: slack_payload.message.blocks[2].elements[0].text.text,
-//       // message_blocks_block_3_elements_1_text_emoji: slack_payload.message.blocks[2].elements[0].text.emoji,
-//       // message_blocks_block_3_elements_1_style: slack_payload.message.blocks[2].elements[0].style,
-//       // message_blocks_block_3_elements_1_value: slack_payload.message.blocks[2].elements[0].value,
-//       // message_blocks_block_3_elements_1_url: slack_payload.message.blocks[2].elements[0].url,
-//       state_values: slack_payload.state.values,
-//       response_url: slack_payload.response_url,
-//       actions_1_action_id: slack_payload.actions[0].action_id,
-//       actions_1_block_id: slack_payload.actions[0].block_id,
-//       actions_1_text_type: slack_payload.actions[0].text.type,
-//       actions_1_text_text: slack_payload.actions[0].text.text,
-//       actions_1_text_emoji: slack_payload.actions[0].text.emoji,
-//       actions_1_value: slack_payload.actions[0].value,
-//       actions_1_style: slack_payload.actions[0].style,
-//       actions_1_type: slack_payload.actions[0].type,
-//       actions_1_action_ts: slack_payload.actions[0].action_ts,
-//     }
-//     console.log('data'+ ' '+ data)
-//     // console.log("actionnnn" + response.json(data))
-//     // console.log(request.body)
-//     // change the url for bubble's url
-//     // axios.post('https://joypool12.bubbleapps.io/version-test/api/1.1/wf/endpoint-rc/initialize', data)
-//     // axios.post('https://joypool12.bubbleapps.io/version-test/api/1.1/wf/endpoint-rc/', data)
-//     axios.post('https://joypool12.bubbleapps.io/version-test/api/1.1/wf/endpoint-action/initialize', data)
-//       .then((res) => {
-//         // console.log(res.headers);
-//         // console.log(`Status: ${res.status}`);
-//         // console.log('Body: action', res.data);
-//       }).catch((err) => {
-//         console.error(err);
-//       });
-//   }
-// )
